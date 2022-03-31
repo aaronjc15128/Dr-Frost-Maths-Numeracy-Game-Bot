330fead001ec537958c8c258d469758370416f19
aaronjc15128 <100516190+aaronjc15128@users.noreply.github.com>
2022-03-31 19:18:08 +0100

    Added dfmbot.py

---
+++ dfmbot.py
@@ -0,0 +1,41 @@
+def main():
+    from time import sleep
+    from os import system
+    
+    system("cls")
+
+    print("LOGIN DETAILS\n-------------")
+    email = input("EMAIL: ")
+    password = input("PASSWORD: ")
+    print("\n\nBOT DETAILS\n-----------")
+    sleeptime = float(input("QUESION DELAY TIME: "))
+    useEnter = bool(input("USE ENTER: "))
+    print("\n\nSELENIUM\n--------")
+
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+
+    driver = webdriver.Chrome('./chromedriver')
+
+    driver.get("https://www.drfrostmaths.com/timestables-game.php")
+
+    driver.find_element(By.NAME, "login-email").send_keys(email)
+    driver.find_element(By.NAME, "login-password").send_keys(password)
+    driver.find_element(By.NAME, "login-password").submit()
+
+    driver.find_element_by_link_text("Start the Clock").click()
+
+    for _ in range(100):
+        sleep(sleeptime)
+        qlist = list(driver.find_element(By.ID, "question").text)
+        if "×" in qlist: qlist[qlist.index("×")] = "*"
+        elif "÷" in qlist: qlist[qlist.index("÷")] = "/"
+        q = "".join(qlist)
+        a = str(int(eval(q)))
+
+        driver.find_element(By.ID, "calculator-display").send_keys(a)
+        if useEnter: driver.find_element(By.ID, "calculator-display").send_keys(Keys.ENTER)
+
+if __name__ == "__main__":
+    main()
